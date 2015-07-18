1dcc517de80d87275653a382b6e77e6f3fd542f9
anshul <anshul@apexol.com>
2015-07-18 20:02:08 +0530

    Features: *Mob Ads display works now *Widget zones based display units can be created. The name of the file if kept same as widget zone will be used otherwise default.html is used *JsonP support added to serve cross site requests *New domain information is added at the time of installation.

---
+++ Extensions/JsonResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Mob.Core.Helpers;
+
+namespace Mob.Core.Extensions
+{
+    public static class JsonResultExtensions
+    {
+        public static JsonpResult ToJsonp(this JsonResult json)
+        {
+            return new JsonpResult { ContentEncoding = json.ContentEncoding, ContentType = json.ContentType, Data = json.Data, JsonRequestBehavior = json.JsonRequestBehavior };
+        }
+    }
+}
---
+++ Helpers/JsonpResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Mob.Core.Helpers
+{
+    public class JsonpResult : JsonResult
+    {
+
+        public JsonpResult()
+        {
+        }
+
+        
+
+        public override void ExecuteResult(ControllerContext controllerContext)
+        {
+            if (controllerContext != null)
+            {
+                HttpResponseBase Response = controllerContext.HttpContext.Response;
+                HttpRequestBase Request = controllerContext.HttpContext.Request;
+
+                string callbackfunction = Request["callback"];
+                if (string.IsNullOrEmpty(callbackfunction))
+                {
+                    throw new Exception("Callback function name must be provided in the request!");
+                }
+                Response.ContentType = "application/x-javascript";
+                if (Data != null)
+                {
+
+                    JavaScriptSerializer serializer = new JavaScriptSerializer();
+                    Response.Write(string.Format("{0}({1});", callbackfunction, serializer.Serialize(this.Data)));
+                }
+            }
+        }
+    }
+}
