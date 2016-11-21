388f405bbb79192740b3706ae4837f44f3094a04
anshul <anshul@apexol.com>
2016-11-21 19:03:18 +0530

    moved common messaging functions and helpers to the core

---
+++ Services/BaseMessageService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nop.Core.Domain.Messages;
+using Nop.Services.Localization;
+using Nop.Services.Messages;
+
+namespace Mob.Core.Services
+{
+    public class BaseMessageService
+    {
+        private readonly ILanguageService _languageService;
+        private readonly IEmailAccountService _emailAccountService;
+        private readonly IMessageTemplateService _messageTemplateService;
+        private readonly ITokenizer _tokenizer;
+        private readonly IQueuedEmailService _queuedEmailService;
+        private readonly EmailAccountSettings _emailAccountSettings;
+
+        public BaseMessageService(ILanguageService languageService, IEmailAccountService emailAccountService, IMessageTemplateService messageTemplateService, ITokenizer tokenizer, IQueuedEmailService queuedEmailService, EmailAccountSettings emailAccountSettings)
+        {
+            _languageService = languageService;
+            _emailAccountService = emailAccountService;
+            _messageTemplateService = messageTemplateService;
+            _tokenizer = tokenizer;
+            _queuedEmailService = queuedEmailService;
+            _emailAccountSettings = emailAccountSettings;
+        }
+
+
+        protected MessageTemplate GetLocalizedActiveMessageTemplate(string messageTemplateName, int storeId)
+        {
+            var messageTemplate = _messageTemplateService.GetMessageTemplateByName(messageTemplateName, storeId);
+
+            //no template found or not active
+            if (messageTemplate == null || !messageTemplate.IsActive)
+                return null;
+
+            return messageTemplate;
+        }
+
+        protected int EnsureLanguageIsActive(int languageId, int storeId)
+        {
+            //load language by specified ID
+            var language = _languageService.GetLanguageById(languageId);
+
+            if (language == null || !language.Published)
+            {
+                //load any language from the specified store
+                language = _languageService.GetAllLanguages(storeId: storeId).FirstOrDefault();
+            }
+            if (language == null || !language.Published)
+            {
+                //load any language
+                language = _languageService.GetAllLanguages().FirstOrDefault();
+            }
+
+            if (language == null)
+                throw new Exception("No active language could be loaded");
+            return language.Id;
+        }
+
+        protected int SendNotification(MessageTemplate messageTemplate,
+                                     EmailAccount emailAccount, int languageId, IEnumerable<Token> tokens,
+                                     string toEmailAddress, string toName)
+        {
+            //retrieve localized message template data
+            var bcc = messageTemplate.GetLocalized((mt) => mt.BccEmailAddresses, languageId);
+            var subject = messageTemplate.GetLocalized((mt) => mt.Subject, languageId);
+            var body = messageTemplate.GetLocalized((mt) => mt.Body, languageId);
+
+            //Replace subject and body tokens 
+            var subjectReplaced = _tokenizer.Replace(subject, tokens, false);
+            var bodyReplaced = _tokenizer.Replace(body, tokens, false);
+
+            var email = new QueuedEmail() {
+                Priority = QueuedEmailPriority.High,
+                From = emailAccount.Email,
+                FromName = emailAccount.DisplayName,
+                To = toEmailAddress,
+                ToName = toName,
+                CC = string.Empty,
+                Bcc = bcc,
+                Subject = subjectReplaced,
+                Body = bodyReplaced,
+                CreatedOnUtc = DateTime.UtcNow,
+                EmailAccountId = emailAccount.Id
+            };
+
+            _queuedEmailService.InsertQueuedEmail(email);
+            return email.Id;
+        }
+
+        protected EmailAccount GetEmailAccountOfMessageTemplate(MessageTemplate messageTemplate, int languageId)
+        {
+            var emailAccounId = messageTemplate.GetLocalized(mt => mt.EmailAccountId, languageId);
+            var emailAccount = _emailAccountService.GetEmailAccountById(emailAccounId);
+            if (emailAccount == null)
+                emailAccount = _emailAccountService.GetEmailAccountById(_emailAccountSettings.DefaultEmailAccountId);
+            if (emailAccount == null)
+                emailAccount = _emailAccountService.GetAllEmailAccounts().FirstOrDefault();
+            return emailAccount;
+
+        }
+    }
+}
